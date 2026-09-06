56c1ee56bca2b691a801a440feaa30fd4435dceb
Gabriel Garcia <gabriel@garciat.com>
2026-09-06 18:30:46 +0200

    log fetch complete

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { index, json, jsx, response, site } from "deno-static/mod.ts";
 
 import { JepIndexSchema } from "@/lib/openjdk-jep/types.ts";
+import { withTimeTag } from "@/lib/timed.ts";
 
 import { SiteConfig } from "./config.ts";
 import { fetchAll } from "./data.ts";
@@ -9,7 +10,10 @@ import { JepHistorySchema } from "./types.ts";
 
 import { HomePage } from "./pages/home.tsx";
 
-const { currentIndex, history } = await fetchAll();
+const { currentIndex, history } = await withTimeTag(
+  fetchAll(),
+  (tag) => console.log(`[fetch]`, `done`, `(${tag})`),
+);
 
 await site(() => ({
   [index]: jsx(<HomePage index={currentIndex} />),
